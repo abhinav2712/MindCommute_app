39e879d48984736ac059d76f65bae0490baca2bd
Retik Singh <griffin@Retiks-MacBook-Air.local>
2022-10-26 20:25:01 +0530

    Changed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
 import express, { Router } from 'express';
+import mysql from 'mysql2';
+import expressLayouts from 'express-ejs-layouts';
 import post from './api/postRoutes.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.set('view engine', 'ejs') // For now.
+app.set('views', `${__dirname}/views`)
+app.set('layout', '/layouts/layout')
+app.set(expressLayouts)
+app.use(express.static('public'));
+
+// Database Setup
+let connect = mysql.createConnection({
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+console.log(connect.error)
+if (connect.error){
+    console.log(`Error: ${err.message}`)
+} else{
+    console.log('Connected to MySQL server.')
+}
 
 // Config properties
 const PORT = 4000;
@@ -12,6 +37,6 @@ const PORT = 4000;
 app.use('/posts/', post)
 
 app.listen(PORT, ()=> {
-    console.log(`MindCommute is live at ${PORT}`)
+    console.log(`MindCommute is live at ${process.env.PORT || PORT}`)
 });
 
